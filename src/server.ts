e53fa4b6aecf13c74661615124db88d32c6169a1
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-02-03 18:39:10 +0600

    prisma setup

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { prisma } from "./lib/prisma"
+import app from "./app"
+const port = 3000
+
+const main=async()=>{
+    try {
+        await prisma.$connect()
+        console.log("connected to databaes sucessfully")
+        app.listen(port, () => {
+            console.log(`Example app listening on port http://localhost:${port}`)
+        })
+    } catch (error) {
+        await prisma.$disconnect()
+        process.exit(1)
+    }
+}
+main()
+
