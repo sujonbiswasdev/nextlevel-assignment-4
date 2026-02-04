435d48392b2803f5839c5abac7441074160bacae
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-02-04 20:30:15 +0600

    category create

---
+++ src/modules/category/category.route.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import auth from "../../middleware/auth";
+import { Roles } from "../../middleware/auth.const";
+
+const router=Router()
+
+export const CategoryRouter={router}
