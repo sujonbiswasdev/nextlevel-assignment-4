78a0f5dc6b2912401aaeea1b5f69612b346c3db6
sujonbiswasdev <sujonbiswasdev@gmail.com>
2026-04-10 23:12:23 +0600

    env.ts setup

---
+++ src/config/env.ts
@@ -0,0 +1,88 @@
+import dotenv from "dotenv";
+import status from "http-status";
+import AppError from "../errorHelper/AppError";
+
+dotenv.config();
+
+interface EnvConfig {
+  DATABASE_URL: string;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
+  FRONTEND_URL: string;
+  PORT: string;
+  ACCESS_TOKEN_SECRET: string;
+  REFRESH_TOKEN_SECRET: string;
+  ACCESS_TOKEN_EXPIRES_IN: string;
+  REFRESH_TOKEN_EXPIRES_IN: string;
+  APP_USER: string;
+  APP_PASS: string;
+  EMAIL: string;
+  PASSWORD: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  NODE_ENV: string;
+  CLOUDINARY: {
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string;
+  };
+
+}
+
+const loadEnvVariables = (): EnvConfig => {
+  const requireEnvVariable = [
+    "DATABASE_URL",
+    "BETTER_AUTH_SECRET",
+    "BETTER_AUTH_URL",
+    "FRONTEND_URL",
+    "PORT",
+    "ACCESS_TOKEN_SECRET",
+    "REFRESH_TOKEN_SECRET",
+    "ACCESS_TOKEN_EXPIRES_IN",
+    "REFRESH_TOKEN_EXPIRES_IN",
+    "APP_USER",
+    "APP_PASS",
+    "EMAIL",
+    "PASSWORD",
+    "GOOGLE_CLIENT_ID",
+    "GOOGLE_CLIENT_SECRET",
+    "NODE_ENV",
+    "CLOUDINARY_CLOUD_NAME",
+    "CLOUDINARY_API_KEY",
+    "CLOUDINARY_API_SECRET",
+  ];
+  requireEnvVariable.forEach((variable) => {
+    if (!process.env[variable]) {
+      throw new AppError(
+        status.INTERNAL_SERVER_ERROR,
+        `Server configuration error: The required environment variable "${variable}" is not set. Verify your .env file or deployment environment settings.`,
+      );
+    }
+  });
+
+  return {
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET as string,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+    PORT: process.env.PORT as string,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+    ACCESS_TOKEN_EXPIRES_IN: process.env.ACCESS_TOKEN_EXPIRES_IN as string,
+    REFRESH_TOKEN_EXPIRES_IN: process.env.REFRESH_TOKEN_EXPIRES_IN as string,
+    APP_USER: process.env.APP_USER as string,
+    APP_PASS: process.env.APP_PASS as string,
+    EMAIL: process.env.EMAIL as string,
+    PASSWORD: process.env.PASSWORD as string,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET as string,
+    NODE_ENV: process.env.NODE_ENV as string,
+    CLOUDINARY: {
+        CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+        CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string
+      },
+  }
+};
+
+export const envVars = loadEnvVariables();
